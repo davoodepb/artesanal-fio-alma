0b796761cfad5927c15aab1cb99f5dfea786a059
Davood <0523059@alunos.epb.pt>
2026-04-13 22:56:50 +0100

    Add SpeedInsights and Analytics components to render

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,16 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <>
+    <App />
+    <SpeedInsights />
+    <Analytics />
+  </>
+);
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', async () => {
